f0e9bba5a720701640ba196016f2e593d418a1e1
G.S.Abhinav <gudipudiabhinav2005@gmail.com>
2025-09-07 19:59:52 +0530

    Update backend auth and other changes

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.6"
 }
 
-group = "com.linkedin"
+group = "com.coding-platform"
 version = "0.0.1-SNAPSHOT"
 
 java {
@@ -16,22 +16,6 @@ java {
 repositories {
 	mavenCentral()
 }
-
-//dependencies {
-//	implementation("org.springframework.boot:spring-boot-starter-web")
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//	implementation("org.springframework.boot:spring-boot-starter-validation")
-//	implementation("org.springframework.boot:spring-boot-starter-mail")
-//	implementation("mysql:mysql-connector-java:8.0.33")
-//
-//	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-//	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
-//	implementation(("io.jsonwebtoken:jjwt-jackson:0.12.6"))
-//
-//	developmentOnly("org.springframework.boot:spring-boot-devtools")
-//	testImplementation("org.springframework.boot:spring-boot-starter-test")
-//	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-//}
 
 
 dependencies {
